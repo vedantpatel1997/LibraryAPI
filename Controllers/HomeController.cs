d12e4a19bb92437d1adf61a9e34522c37b75c508
vedantpatel1997 <vedantp9@gmail.com>
2025-03-08 12:21:29 -0700

    2.0.7 add path for /

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = "Backend is running. Access the Swagger UI for API documentation: ";
+            var swaggerUrl = $"{Request.Scheme}://{Request.Host}/swagger/index.html";
+            var response = $"{message}<a href='{swaggerUrl}'>{swaggerUrl}</a>";
+
+            return new ContentResult
+            {
+                Content = response,
+                ContentType = "text/html"
+            };
+        }
+    }
+}
